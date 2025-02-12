81bbd39fc15eac393f446ff34820559e18df882d
samipevekar <samipevekar499@gmail.com>
2025-02-12 12:18:48 +0530

    fix catch-all-segment

--- routing-demo/src/app/docs/[[...slug]]/page.tsx
+++ routing-demo/src/app/docs/[[...slug]]/page.tsx
@@ -1,25 +1,52 @@
-import Link from "next/link"
+import Link from "next/link";
 
-export default async function page({params}:{
-    params:{slug:string[]}
+export default async function Doc({
+  params,
+}: {
+  params: Promise<{ slug: string[] }>;
 }) {
 
+  const { slug } = await params;
+
+  if (slug?.length === 2) {
+    return (
+      <>
+      <h1>
+        Viewing docs for feature {slug[0]} and concept {slug[1]}
+      </h1>
+      <Link href={'/docs'}>docs</Link>
+      </>
+    );
+  } else if (slug?.length === 1) {
+    const con:number[] = [1,2,3,4,5]
+    return (
+      <>
+        <h1>Viewing docs for feature {slug[0]}</h1>
+        <h2>concepts</h2>
+        
+        {con.map(con=>{
+          return (
+            <div key={con}>
+              <Link  href={`/docs/${slug[0]}/${con}`}>{con}</Link>
+            </div>
+
+          )
+        })}
+      </>
+    )
+
+  }
+
   const arr:number[] = [1,2,3,4,5]
-
-    const {slug} = await params
-
-
   return (
-    <div>
-      <h1>Concept</h1>    
-      {slug.length==2 && <h1>Viewing docs for feature {slug[0]} and concept {slug[1]} </h1> }
-      {slug.length==1 && <h1>Viewsing docs for feature {slug[0]}  </h1> }
-      {arr.map(elt=>(
+    <>
+      <h1>Docs home page</h1>
+      {arr.map(elt=>
         <div key={elt}>
-          <Link href={`/docs/${slug[0]}/${elt}`}>concept {elt}</Link>
+         <Link  href={`/docs/${elt}`}>{elt}</Link>
         </div>
-      ))}
-
-    </div>
+      )}
+    </>
+    
   )
 }
