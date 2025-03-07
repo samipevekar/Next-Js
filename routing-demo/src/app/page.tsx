322389ae07d8b82ee7650077ad554287155cded6
samipevekar <samipevekar499@gmail.com>
2025-03-07 10:14:27 +0530

    all about routing is completed

--- routing-demo/src/app/page.tsx
+++ routing-demo/src/app/page.tsx
@@ -8,6 +8,8 @@ export default function page() {
       <Link className="underline text-purple-900" style={{display:"block"}} href={'/about'}>About</Link>
       <Link className="underline text-purple-900" style={{display:"block"}} href={'/docs'}>Docs</Link>
       <Link className="underline text-purple-900" href={'/blog'}>Blog</Link>
+      <Link href={"/articles/breaking-news-123?lang=en"}>Read in English</Link>
+      <Link href={"/articles/breaking-news-123?lang=fr"}>Read in French</Link>
     </div>
   )
 }
